85efd0a2c360a025ca71ae4cff4bcfd820a13ea3
imbekrishna <imbekrishna@gmail.com>
2023-10-25 17:53:31 +0530

    part10-ex-10.5 done

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/Main';
+import { StatusBar } from 'react-native';
+import { NativeRouter } from 'react-router-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>App created with expo!</Text>
+    <>
+      <NativeRouter>
+        <Main />
+      </NativeRouter>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/theme.js
@@ -0,0 +1,38 @@
+import { Platform } from 'react-native';
+
+function getFont() {
+  switch (Platform.OS) {
+    case 'android':
+      return 'Roboto';
+    case 'ios':
+      return 'Arial';
+    default:
+      return 'System';
+  }
+}
+
+const theme = {
+  colors: {
+    textPrimary: '#24292e',
+    textSecondary: '#586069',
+    primary: '#0366d6',
+    background: '#24292e',
+    mainBackground: '#e1e4e8',
+    error: '#d73a4a',
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+    heading: 18,
+    title: 24,
+  },
+  fonts: {
+    main: getFont(),
+  },
+  fontWeights: {
+    normal: '400',
+    bold: '700',
+  },
+};
+
+export default theme;
