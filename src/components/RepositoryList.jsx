0bc955790b69febe92ae9e407f419de164d473c8
imbekrishna <imbekrishna@gmail.com>
2023-10-26 23:58:06 +0530

    part10-ex-10.23 done | add signup form

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,12 +2,67 @@ import { View, StyleSheet, FlatList, Pressable } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import useRepository from '../hooks/useRepositories';
 import { useNavigate } from 'react-router-native';
+import { Button, Menu } from 'react-native-paper';
+import theme from '../theme';
+import { useState } from 'react';
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
+  formContainer: {
+    backgroundColor: theme.colors.formBackground,
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colors.background,
+  },
 });
+
+const FilterHeader = ({ refetch }) => {
+  const [visible, setVisible] = useState(false);
+  const [buttonTitle, setButtonTitle] = useState('Latest Repositories');
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+
+  const handleClick = async (title, orderBy, orderDirection) => {
+    setButtonTitle(title);
+    closeMenu();
+    await refetch({
+      orderBy,
+      orderDirection,
+    });
+  };
+
+  return (
+    <View style={styles.formContainer}>
+      <Menu
+        visible={visible}
+        onDismiss={closeMenu}
+        anchor={<Button onPress={openMenu}>{buttonTitle}</Button>}
+      >
+        <Menu.Item title="Select an item..." disabled />
+        <Menu.Item
+          onPress={() =>
+            handleClick('Latest Repositories', 'CREATED_AT', 'DESC')
+          }
+          title="Latest Repositories"
+        />
+        <Menu.Item
+          onPress={() =>
+            handleClick('Highest rated repositories', 'RATING_AVERAGE', 'DESC')
+          }
+          title="Highest rated repositories"
+        />
+        <Menu.Item
+          onPress={() => {
+            handleClick('Lowest rated repositories', 'RATING_AVERAGE', 'ASC');
+          }}
+          title="Lowest rated repositories"
+        />
+      </Menu>
+    </View>
+  );
+};
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
@@ -32,9 +87,14 @@ export const RepositoryListContainer = ({ repositories }) => {
 };
 
 const RepositoryList = () => {
-  const { repositories } = useRepository();
+  const { repositories, refetch } = useRepository();
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return (
+    <View>
+      <FilterHeader refetch={refetch} />
+      <RepositoryListContainer repositories={repositories}/>
+    </View>
+  );
 };
 
 export default RepositoryList;
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,13 +1,17 @@
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = () => {
+const useRepositories = (orderBy = 'CREATED_AT', orderDirection = 'ASC') => {
   const { data, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    variables: {
+      orderDirection: orderDirection,
+      orderBy: orderBy,
+    },
     fetchPolicy: 'cache-and-network',
   });
 
-  if(loading){
-    return {}
+  if (loading) {
+    return {};
   }
 
   return { repositories: data.repositories, loading, refetch };
